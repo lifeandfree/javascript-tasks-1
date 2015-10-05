7c8d17e7e75aa6aa0a2be3256673ed81e072491c
Сухачев Илья <lifeandfree@gmail.com>
2015-10-05 12:02:00 +0100

    Исправил ошибки

--- roman-time.js
+++ roman-time.js
@@ -8,16 +8,16 @@ function transferTimeToRomanSystem(hours, minutes) {
     hours = parseInt(hours, 10);
     minutes = parseInt(minutes, 10);
     if (isNaN(hours) || isNaN(minutes)) {
-        console.error('Время указано не верно.' + '\n'
-                + 'Ошибка: Введеные параметры не являются числовыми значениями.' + '\n');
+        console
+                .error('Время указано не верно. Ошибка: Введеные параметры не являются числовыми значениями.');
         return false;
     }
     
     if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59) {
-        console.error('Время указано не верно' + '\n');
+        console.error('Время указано не верно');
     }
     else {
-        console.log((integerToRoman(hours) + ':' + integerToRoman(minutes) + '\n'));
+        console.log(integerToRoman(hours) + ':' + integerToRoman(minutes));
     }
 }
 
@@ -25,19 +25,14 @@ function integerToRoman(n) {
     if (n == 0) {
         return '-';
     }
-    var a = [
-            'I', 'IV', 'V', 'IX', 'X', 'XL', 'L'
-    ];
-    var b = [
-            1, 4, 5, 9, 10, 40, 50
-    ];
-    var s = "";
+    var a = [ 'I', 'IV', 'V', 'IX', 'X', 'XL', 'L' ];
+    var b = [ 1, 4, 5, 9, 10, 40, 50 ];
+    var s = '';
     for (var i = a.length - 1; i >= 0; i--) {
         while (n >= b[i]) {
             s += a[i];
             n -= b[i];
         }
-        ;
     }
     return s;
 }
