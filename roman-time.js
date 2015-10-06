ac51cb8fffa7a524ea97f4c03cb41a7cf05cfed6
Сухачев Илья <lifeandfree@gmail.com>
2015-10-06 03:37:08 +0100

    Исправил ошибки по псевдографики

--- roman-time.js
+++ roman-time.js
@@ -2,140 +2,132 @@
 var hours = process.argv[2];
 var minutes = process.argv[3];
 
-transferTimeToRomanSystem(hours, minutes);
+var formatRomanNumbers = 'ROMAN';
+var formatRomanNumbersASCII = 'ASCII';
+var romanNumbers = [ 'I', 'IV', 'V', 'IX', 'X', 'XL', 'L' ];
+var arabicNumbers = [ 1, 4, 5, 9, 10, 40, 50 ];
+var numbersASCII = [[ ' * * * * ',
+                      '   * *   ',
+                      '   * *   ',
+                      '   * *   ',
+                      '   * *   ',
+                      '   * *   ',
+                      ' * * * * '
+                      ],
+                      [' * * * *  *           * ',
+                       '   * *     *         *  ',
+                       '   * *      *       *   ',
+                       '   * *       *     *    ',
+                       '   * *        *   *     ',
+                       '   * *         * *      ',
+                       ' * * * *        *       '
+                       ],
+                       [' *           * ',
+                        '  *         *  ',
+                        '   *       *   ',
+                        '    *     *    ',
+                        '     *   *     ',
+                        '      * *      ',
+                        '       *       '
+                        ],
+                       [' * * * *  *     * ',
+                        '   * *     *   *  ',
+                        '   * *      * *   ',
+                        '   * *       *    ',
+                        '   * *      * *   ',
+                        '   * *     *   *  ',
+                        ' * * * *  *     * '
+                        ],
+                       [' *     * ',
+                        '  *   *  ',
+                        '   * *   ',
+                        '    *    ',
+                        '   * *   ',
+                        '  *   *  ',
+                        ' *     * '
+                        ],
+                       [' *     *   *       ',
+                        '  *   *    *       ',
+                        '   * *     *       ',
+                        '    *      *       ',
+                        '   * *     *       ',
+                        '  *   *    *       ',
+                        ' *     *   * * * * '
+                        ],
+                       [' *       ',
+                        ' *       ',
+                        ' *       ',
+                        ' *       ',
+                        ' *       ',
+                        ' *       ',
+                        ' * * * * '
+                        ] ];
+var colonASCII = [ '          ',
+                   '          ',
+                   '    * *   ',
+                   '          ',
+                   '    * *   ',
+                   '          ',
+                   '          ' ];
+var zeroASCII = [ '          ',
+                '          ',
+                '          ',
+                '    ---   ',
+                '          ',
+                '          ',
+                '          ',
+                ];
 
-function transferTimeToRomanSystem(hours, minutes) {
-    hours = parseInt(hours, 10);
-    minutes = parseInt(minutes, 10);
-    if (isNaN(hours) || isNaN(minutes)) {
-        console.error('Время указано не верно. Ошибка: Введеные параметры не являются числовыми значениями.');
-        return false;
-    }
-    
+hours = parseInt(hours, 10);
+minutes = parseInt(minutes, 10);
+if (isNaN(hours) || isNaN(minutes)) {
+    console.error('Время указано не верно. Ошибка: Введеные параметры не являются числовыми значениями.');
+}
+else {
     if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59) {
         console.error('Время указано не верно');
     }
     else {
-        console.log(integerToRoman(hours) + ':' + integerToRoman(minutes));
-        
-        var t = integerToRomanInASCII(hours);
-        t.push([ '          ',
-                 '          ',
-                 '    * *   ',
-                 '          ',
-                 '    * *   ',
-                 '          ',
-                 '          ',
-                 ]);
-        t = t.concat(integerToRomanInASCII(minutes));
-        var r = '';
+        console.log(arrayToString(integerToRoman(hours, romanNumbers, formatRomanNumbers)) + ':'
+                + arrayToString(integerToRoman(minutes, romanNumbers, formatRomanNumbers)));
+        var t = integerToRoman(hours, numbersASCII, formatRomanNumbersASCII);
+        t.push(colonASCII);
+        t = t.concat(integerToRoman(minutes, numbersASCII, formatRomanNumbersASCII));
+        var resultASCII = '';
         for (var i = 0; i < t[0].length; i++) {
             for (var j = 0; j < t.length; j++) {
-                r += t[j][i];
+                resultASCII += t[j][i];
             }
-            r += '\n';
+            resultASCII += '\n';
         }
-        console.log(r);
+        console.log(resultASCII);
     }
 }
 
-function integerToRoman(n) {
-    if (n == 0) {
-        return '-';
-    }
-    var a = [ 'I', 'IV', 'V', 'IX', 'X', 'XL', 'L' ];
-    var b = [ 1, 4, 5, 9, 10, 40, 50 ];
-    var s = '';
-    for (var i = a.length - 1; i >= 0; i--) {
-        while (n >= b[i]) {
-            s += a[i];
-            n -= b[i];
+function integerToRoman(value, numbers, outputFormat) {
+    if (value == 0) {
+        if (outputFormat == formatRomanNumbers) {
+            return [ '-' ];
+        }
+        if (outputFormat == formatRomanNumbersASCII) {
+            return [ zeroASCII ];
         }
     }
-    return s;
-}
-
-function integerToRomanInASCII(n) {
-    if (n == 0) {
-        return [ [ '          ',
-                 '          ',
-                 '          ',
-                 '    ---   ',
-                 '          ',
-                 '          ',
-                 '          ',
-                 ] ];
-    }
-    var c = [];
-    c.push([ ' * * * * ',
-             '   * *   ',
-             '   * *   ',
-             '   * *   ',
-             '   * *   ',
-             '   * *   ',
-             ' * * * * ',
-             ]);
-
-    c.push([' * * * *  *           * ',
-            '   * *     *         *  ',
-            '   * *      *       *   ',
-            '   * *       *     *    ',
-            '   * *        *   *     ',
-            '   * *         * *      ',
-            ' * * * *        *       ',
-            ]);
-
-    c.push([' *           * ',
-            '  *         *  ',
-            '   *       *   ',
-            '    *     *    ',
-            '     *   *     ',
-            '      * *      ',
-            '       *       ',
-            ]);
-
-    c.push([' * * * *  *     * ',
-            '   * *     *   *  ',
-            '   * *      * *   ',
-            '   * *       *    ',
-            '   * *      * *   ',
-            '   * *     *   *  ',
-            ' * * * *  *     * ',
-            ]);
-
-    c.push([' *     * ',
-            '  *   *  ',
-            '   * *   ',
-            '    *    ',
-            '   * *   ',
-            '  *   *  ',
-            ' *     * ',
-            ]);
-
-    c.push([' *     *   *       ',
-            '  *   *    *       ',
-            '   * *     *       ',
-            '    *      *       ',
-            '   * *     *       ',
-            '  *   *    *       ',
-            ' *     *   * * * * ',
-            ]);
-
-    c.push([' *       ',
-            ' *       ',
-            ' *       ',
-            ' *       ',
-            ' *       ',
-            ' *       ',
-            ' * * * * ',
-            ]);
-    var b = [ 1, 4, 5, 9, 10, 40, 50 ];
-    var s = [];
-    for (var i = c.length - 1; i >= 0; i--) {
-        while (n >= b[i]) {
-            s.push(c[i]);
-            n -= b[i];
+    
+    var result = [];
+    for (var i = numbers.length - 1; i >= 0; i--) {
+        while (value >= arabicNumbers[i]) {
+            result.push(numbers[i]);
+            value -= arabicNumbers[i];
         }
     }
-    return s;
+    return result;
 }
+
+function arrayToString(array) {
+    var r = '';
+    for (var i = 0; i < array.length; i++) {
+        r += array[i];
+    }
+    return r;
+};
